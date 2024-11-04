a39d1b0966ad51dbc35e27ab713f9ef2608aaee3
taha12701 <108870371+taha12701@users.noreply.github.com>
2024-11-04 23:47:46 +0500

    Add files via upload

--- p23.py
+++ p23.py
@@ -1,11 +1,13 @@
 class items:
   pay_rate=0.90
+  all=[]
 
   def __init__(self,name:str,price:int,quantity:int):
     self.name=name
     self.price=price
     self.quantity=quantity
 
+    items.all.append(self)
 
   def actual_amount(self):
     self.act_amt=self.price * self.quantity
@@ -18,16 +20,29 @@ class items:
   def amount_detect(self):
     self.amount=self.act_amt - self.disc_amt
     print(f"The amount detect from after discount is : $ {self.amount:.2f}")
+
+
+  def __repr__(self):
+    return(f"items : '{self.name}',{self.price},{self.quantity}")
     
 
 
 i1=items("Atomic Habits",35,8)
-i1.actual_amount()
-i1.discount_amount()
-i1.amount_detect()
+# i1.actual_amount()
+# i1.discount_amount()
+# i1.amount_detect()
 
 i2=items("State of Overconfidence",39,17)
-i2.actual_amount()
-items.pay_rate=0.87
-i2.discount_amount()
-i2.amount_detect()
+i3=items("English Dictionary",39,17)
+i4=items("Cricket Stadium",39,17)
+i5=items("Art of Overconfidence",39,17)
+# i2.actual_amount()
+# items.pay_rate=0.87
+# i2.discount_amount()
+# i2.amount_detect()
+
+# print(items.all)
+
+
+for instance in items.all:
+  print(instance.name)
